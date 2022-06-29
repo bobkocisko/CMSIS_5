ec985095b83e1b88b0dec7f4afcac28d1287439b
Bob Kocisko <rkocisko@gmail.com>
2022-06-29 17:53:58 +0000

    Simplify and add support for intrinsics like qsub

--- CMSIS/DSP/configCore.cmake
+++ CMSIS/DSP/configCore.cmake
@@ -34,8 +34,7 @@ function(configcore PROJECTNAME ROOT)
 
   # Going Rogue!
   if (ARM_CPU  MATCHES  "^cortex-a76.cortex-a55$" )
-    target_include_directories(${PROJECTNAME} PUBLIC "${ROOT}/CMSIS/Core_A/Include")
-    target_compile_definitions(${PROJECTNAME} PRIVATE ARM_MATH_DSP) 
+    target_compile_definitions(${PROJECTNAME} PRIVATE __GNUC_PYTHON__) 
     SET(CORTEXM OFF)
     SET(CORTEXA ON)
     SET(CORTEXR OFF)
